cc6ed36be9b2404e8810a10768870701fad9f444
Dmytro Motuzko <motuzko.d15@gmail.com>
2021-03-12 14:08:54 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    if (n > 0) {
+        aStr = String(n).split('').reverse().join('');
+        return aStr;
+    } else {
+        aStr = -n;
+        aStr = String(-n).split('').reverse().join('');
+        return aStr;
+    }
+
 }
